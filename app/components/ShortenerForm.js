20ccf668318b6a4165aa55d32fe96e4372e59bdf
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-04 17:34:55 +0600

    refactor: move URL shortening logic to a separate component

---
+++ app/components/ShortenerForm.js
@@ -0,0 +1,51 @@
+"use client";
+import { Link2 } from "lucide-react";
+import React, { useState } from "react";
+
+const ShortenerForm = () => {
+  const [url, setUrl] = useState("");
+  const [shortenedUrl, setShortenedUrl] = useState("");
+
+  async function shortURL(e) {
+    e.preventDefault();
+    const response = await fetch(`
+    https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    if (response.ok) {
+      const data = await response.text();
+      setShortenedUrl(data);
+    } else {
+      alert("Error shortening URL");
+    }
+  }
+  return (
+    <div className="w-full">
+      {/* Form */}
+      <form
+        onSubmit={shortURL}
+        className="flex justify-center items-center w-full"
+      >
+        <input
+          className="p-2 border border-gray-300 rounded-md text-sm w-6/12"
+          type="text"
+          placeholder="Enter URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2 flex items-center justify-center gap-1 text-sm"
+          type="submit"
+        >
+          Shorten <Link2 className="size-4" />
+        </button>
+      </form>
+      {shortenedUrl && (
+        <div className="bg-blue-100 p-4 mt-4 rounded-md w-9/12 text-center space-y-3">
+          <p className="font-semibold">Shortened URL:</p>
+          <p className="text-copy break-all">{shortenedUrl}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShortenerForm;
--- app/page.js
+++ app/page.js
@@ -1,64 +1,16 @@
-"use client";
-import { Link2 } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+import ShortenerForm from "./components/ShortenerForm";
 
 export default function Home() {
-  const [url, setUrl] = useState("");
-  const [shortenedUrl, setShortenedUrl] = useState("");
-
-  async function shortURL(e) {
-    e.preventDefault();
-    const response = await fetch(`
-https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
-    if (response.ok) {
-      const data = await response.text();
-      setShortenedUrl(data);
-    } else {
-      alert("Error shortening URL");
-    }
-  }
   return (
     <>
       <div className="grid grid-cols-2 h-screen">
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen w-full space-y-6">
           <div className="text-center">
             <h3 className="text-5xl font-bold tracking-tight">Link Shortner</h3>
-            <p className="text-lg mt-4">Shorten your links with ease</p>
-            <p> </p>
+            <p className="text-sm">Shorten your links with ease</p>
           </div>
-          {/* Form */}
-          <form
-            onSubmit={shortURL}
-            className="flex justify-center items-center w-full"
-          >
-            <input
-              className="p-2 border border-gray-300 rounded-md text-sm w-6/12"
-              type="text"
-              placeholder="Enter URL"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2 flex items-center justify-center gap-1 text-sm"
-              type="submit"
-            >
-              Shorten <Link2 className="size-4" />
-            </button>
-          </form>
-          {shortenedUrl && (
-            <div className="bg-blue-100 p-4 mt-4 rounded-md w-9/12 text-center space-y-3">
-              <p className="font-semibold">Shortened URL:</p>
-              <Link
-                href={shortenedUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {shortenedUrl}
-              </Link>
-            </div>
-          )}
+          <ShortenerForm></ShortenerForm>
         </div>
         {/* Featured Image */}
         <div className="h-screen">
