fb09a553d0885cb3d28d22dc5375aadd231af135
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-04 17:03:57 +0600

    add image component and configure remote patterns for Unsplash

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
@@ -16,6 +18,14 @@ export default function Home() {
             Shorten
           </button>
         </div>
+      </div>
+      <div>
+        <Image
+          src="https://images.unsplash.com/photo-1678924665955-5dcccbf904f7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Link Shortner Image"
+          width={700}
+          height={1000}
+        />
       </div>
     </>
   );
