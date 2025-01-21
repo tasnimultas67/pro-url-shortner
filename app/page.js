cc992280a723c337955a268d7cf0d6a16b349621
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-21 13:47:04 +0600

    feat: add SparklesText component for animated text effects and integrate it into the homepage

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import ShortenerForm from "./components/ShortenerForm";
 import Footer from "./components/Footer/Footer";
 import QrCodeGenerator from "./components/QrCodeGenerator";
+import SparklesText from "@/components/ui/sparkles-text";
 
 export default function Home() {
   return (
@@ -9,9 +10,10 @@ export default function Home() {
       <div className="bg-slate-100">
         <div className="flex flex-col items-center justify-center h-[100dvh] w-full space-y-6">
           <div className="text-center">
-            <h3 className="text-6xl font-semibold tracking-tight bg-clip-text text-transparent bg-gradient-to-t from-black to-blue-800">
+            {/* <h3 className="text-6xl font-semibold tracking-tight bg-clip-text text-transparent bg-gradient-to-t from-black to-blue-800">
               Shorten your looong URL :)
-            </h3>
+            </h3> */}
+            <SparklesText text="Shorten your looong URL :)" />
             <p className="text-sm ">Shorten your links with ease</p>
           </div>
           <div className="w-11/12 md:w-7/12 m-auto bg-white p-5 md:p-8 md:pb-2 rounded-lg shadow-xl">
