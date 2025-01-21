3c7409a8dc6219087a09376805a7e156f8c438dc
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-21 13:30:49 +0600

    fix: add hover effect to buttons in ShortenerForm for improved user interaction

--- app/components/ShortenerForm.js
+++ app/components/ShortenerForm.js
@@ -65,7 +65,7 @@ const ShortenerForm = () => {
           onChange={(e) => setUrl(e.target.value)}
         />
         <button
-          className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2 flex items-center justify-center gap-1 text-sm"
+          className="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md ml-2 flex items-center justify-center gap-1 text-sm"
           type="submit"
         >
           Shorten <Link2 className="size-4" />
@@ -92,7 +92,7 @@ const ShortenerForm = () => {
                 </Link>
               </div>
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                className="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md"
                 onClick={() => {
                   navigator.clipboard.writeText(shortenedUrl);
                   alert("Copied to clipboard");
