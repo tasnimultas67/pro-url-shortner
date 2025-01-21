dd8c0fe6620f9a6049bfc2436d6eaf1ad28b198f
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-21 13:31:33 +0600

    fix: add title attribute to button in ShortenerForm for improved accessibility

--- app/components/ShortenerForm.js
+++ app/components/ShortenerForm.js
@@ -92,7 +92,8 @@ const ShortenerForm = () => {
                 </Link>
               </div>
               <button
-                className="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md"
+                title="Copy to clipboard"
+                className="bg-blue-500 hover:bg-blue-800 text-white p-2 rounded-md"
                 onClick={() => {
                   navigator.clipboard.writeText(shortenedUrl);
                   alert("Copied to clipboard");
