f2d3f613cd75d3b8dd16db5f9bed67e718c0967b
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-23 23:21:09 +0600

    fix: adjust indentation and formatting in Home page layout for consistency

--- app/page.js
+++ app/page.js
@@ -18,15 +18,15 @@ export default function Home() {
           height={800}
           width={2600}
         />
-        <div className=" py-10 md:py-0 z-[100] absolute inset-0 flex flex-col items-center justify-center">
-          <div className="flex flex-col items-center justify-center m-auto space-y-6 ">
-            <div className="text-center">
-              <SparklesText text="Shorten your looong URL :)" />
-              <p className="text-sm ">Shorten your links with ease</p>
-            </div>
-            <div className="w-11/12 md:w-7/12 m-auto bg-white p-5 md:p-8 md:pb-2 rounded-lg shadow-xl">
-              <ShortenerForm></ShortenerForm>
-            </div>
+      </div>
+      <div className="  py-10 md:py-0 z-[100] absolute inset-0 flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center m-auto space-y-6 ">
+          <div className="text-center">
+            <SparklesText text="Shorten your looong URL :)" />
+            <p className="text-sm ">Shorten your links with ease</p>
+          </div>
+          <div className="w-11/12 md:w-7/12 m-auto bg-white p-5 md:p-8 md:pb-2 rounded-lg shadow-xl">
+            <ShortenerForm></ShortenerForm>
           </div>
         </div>
       </div>
