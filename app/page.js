d1f4ddddecaf24bc9c1b2c8954c0b4a323731f8a
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-23 23:15:36 +0600

    fix: update FlickeringGrid positioning and styling for improved layout on Home page

--- app/page.js
+++ app/page.js
@@ -9,26 +9,27 @@ export default function Home() {
     <>
       <div className="relative h-screen w-full overflow-hidden rounded-lg border bg-background py-20">
         <FlickeringGrid
-          className="absolute inset-0 z-0 size-full"
+          className="fixed inset-0 z-0 size-full"
           squareSize={4}
-          gridGap={6}
+          gridGap={1}
           color="#6B7280"
-          maxOpacity={0.5}
-          flickerChance={0.1}
+          maxOpacity={0.4}
+          flickerChance={0.2}
           height={800}
           width={2600}
         />
-        <div className="flex flex-col items-center justify-center m-auto space-y-6 z-[1000] absolute inset-0">
-          <div className="text-center">
-            <SparklesText text="Shorten your looong URL :)" />
-            <p className="text-sm ">Shorten your links with ease</p>
-          </div>
-          <div className="w-11/12 md:w-7/12 m-auto bg-white p-5 md:p-8 md:pb-2 rounded-lg shadow-xl">
-            <ShortenerForm></ShortenerForm>
+        <div className=" py-10 md:py-0 z-[100] absolute inset-0 flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center m-auto space-y-6 ">
+            <div className="text-center">
+              <SparklesText text="Shorten your looong URL :)" />
+              <p className="text-sm ">Shorten your links with ease</p>
+            </div>
+            <div className="w-11/12 md:w-7/12 m-auto bg-white p-5 md:p-8 md:pb-2 rounded-lg shadow-xl">
+              <ShortenerForm></ShortenerForm>
+            </div>
           </div>
         </div>
       </div>
-      {/* <div className="bg-slate-100 py-10 md:py-0"></div> */}
     </>
   );
 }
