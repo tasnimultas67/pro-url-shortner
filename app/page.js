534c0939ce7198df8e836ff16d8e688b05c7a79d
Tasnimul Haque <tasnimul.haque6@gmail.com>
2025-01-23 21:42:54 +0600

    fix: update layout and styling in Home and SparklesText components for improved responsiveness and readability

--- app/page.js
+++ app/page.js
@@ -7,12 +7,9 @@ import SparklesText from "@/components/ui/sparkles-text";
 export default function Home() {
   return (
     <>
-      <div className="bg-slate-100">
-        <div className="flex flex-col items-center justify-center h-[100dvh] w-full space-y-6">
+      <div className="bg-slate-100 py-10 md:py-0">
+        <div className="flex flex-col items-center justify-center md:h-[100dvh] w-full space-y-6">
           <div className="text-center">
-            {/* <h3 className="text-6xl font-semibold tracking-tight bg-clip-text text-transparent bg-gradient-to-t from-black to-blue-800">
-              Shorten your looong URL :)
-            </h3> */}
             <SparklesText text="Shorten your looong URL :)" />
             <p className="text-sm ">Shorten your links with ease</p>
           </div>
